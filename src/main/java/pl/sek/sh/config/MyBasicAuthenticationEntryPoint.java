c4fda0675e162c07b9fd4a5fcc8a7a2c48373c24
Filip Sęk <filipsek@live.com>
2019-09-10 15:57:02 +0200

    FIX: Basic Spring authorization, csrf disabled

--- src/main/java/pl/sek/sh/config/MyBasicAuthenticationEntryPoint.java
+++ src/main/java/pl/sek/sh/config/MyBasicAuthenticationEntryPoint.java
@@ -5,16 +5,18 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
  
     @Override
-    public void commence (HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) {
-        response.addHeader("WWW-Authenticate", "Basic realm = " + getRealmName() + " ");
+    public void commence (HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm = " + getRealmName() + "");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         log.info("Unauthorized login attempt");
     }
--- src/main/java/pl/sek/sh/config/WebSecurityConfig.java
+++ src/main/java/pl/sek/sh/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -34,19 +35,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint);
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         httpSecurity.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
